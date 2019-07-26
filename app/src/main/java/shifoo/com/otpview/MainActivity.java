6e166d18785fd379f20a75a949c5914065e11dce
Dell <vinodsharma9643@gmail.com>
2019-07-26 10:20:07 +0530

    Button Added in Otp Xml

--- app/src/main/java/shifoo/com/otpview/MainActivity.java
+++ app/src/main/java/shifoo/com/otpview/MainActivity.java
@@ -1,24 +1,35 @@
 package shifoo.com.otpview;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.goodiebag.pinview.Pinview;
 
 public class MainActivity extends AppCompatActivity {
 Pinview pinview;
+Button Btn;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Btn= (Button)findViewById(R.id.btn);
+
         pinview=findViewById(R.id.mypinview);
 
         pinview.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onDataEntered(Pinview pinview, boolean fromUser) {
                 Toast.makeText(MainActivity.this, pinview.getValue(), Toast.LENGTH_SHORT).show();
+                Btn.setBackground(getDrawable(R.drawable.round));
+
             }
         });
     }
